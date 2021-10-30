9c9ab1888707a2b994592c18f293ba04c9f52093
Lisa <redlisa125@gmail.com>
2021-10-30 13:37:46 -1000

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+
+def greet_programmer 
+  puts "Hello, programmer!"
+end
+greet_programmer
+
+
+def greet name 
+  puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+
+def greet_with_default name = "programmer" 
+    puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default "Sunny"
+
+
+def add num1, num2  
+  num1 + num2
+end
+sum = add 1, 2
+sum
+
+
+def halve number
+    number / 2
+    nil
+end   
+result = halve 4
+result = halve "two"
+
